788c6531c98df8a206637fcc91e6fd6f4d115dbc
matthewtwarren <mattwarren95@gmail.com>
2021-07-04 19:27:00 +0100

    Fixed convert key function

--- src/playlist.py
+++ src/playlist.py
@@ -13,11 +13,15 @@ class Playlist:
         print()
         print("Your Spotify playlists:\n")
         user_playlists = self.get_spotify_playlists(user_account, print_switch=True)
-        user_selection = input("Enter the number(s) of the playlists to be added (e.g. 0,1,3) or ALL: ")
+        user_selection = input("\nEnter the number(s) of the playlists to be added (e.g. 0,1,3) or ALL: ")
         if user_selection.lower() == "all":
             index_selection = list(range(len(user_playlists)))
         else:
-            index_selection = [int(x)-1 for x in user_selection.split(",")]
+            try:
+                index_selection = [int(x)-1 for x in user_selection.split(",")]
+            except ValueError:
+                print("Invalid selection. Playlists must be selected using the number displayed to the left of their name.\n")
+                return
         for idx, playlist_name in enumerate(user_playlists):
             if idx in index_selection:
                 self.add_tracks(user_account, user_playlists[playlist_name])
@@ -71,8 +75,11 @@ class Playlist:
         """
         filtered_tracks = []
         for track in self._tracks:
-            spotify_key = user_account._auth_sp.audio_features([track])[0]['key']
-            camelot_key = convert_key(spotify_key)
+            spotify_key = (
+                user_account._auth_sp.audio_features([track])[0]['key'],
+                user_account._auth_sp.audio_features([track])[0]['mode']
+                )
+            camelot_key = self.convert_key(spotify_key)
             if camelot_key in filter:
                 filtered_tracks.append(track)
 
@@ -90,15 +97,18 @@ class Playlist:
             ascending_order: Whether the tracks should be sorted in ascending (T) or descending (F) order.
         """
         track_tempos = []
-        for track in self._tracks:
-            track_tempos.append(user_account._auth_sp.audio_features([track])[0]['tempo'])
-        sorted_tracks = [track for _, track in sorted(zip(track_tempos, self._tracks))]
-        if ascending_order:
-            self._tracks = sorted_tracks
-            print("Tracks sorted in ascending order by tempo (BPM).\n")
+        if len(self._tracks) > 0:
+            for track in self._tracks:
+                track_tempos.append(user_account._auth_sp.audio_features([track])[0]['tempo'])
+            sorted_tracks = [track for _, track in sorted(zip(track_tempos, self._tracks))]
+            if ascending_order:
+                self._tracks = sorted_tracks
+                print("Tracks sorted in ascending order by tempo (BPM).\n")
+            else:
+                self._tracks = sorted_tracks.reverse()
+                print("Tracks sorted in descending order by tempo (BPM).\n")
         else:
-            self._tracks = sorted_tracks.reverse()
-            print("Tracks sorted in descending order by tempo (BPM).\n")
+            print("Your playlist is empty. Add to the playlist before sorting.\n")
 
     def add_to_spotify(self,user_account):
         """Adds the playlist to the users Spotify account.
@@ -130,32 +140,33 @@ class Playlist:
             key: Spotify key value (str)
         """
         key_dict = {
-            '0,1':'8B',
-            '1,1':'3B',
-            '2,1':'10B',
-            '3,1':'5B',
-            '4,1':'12B',
-            '5,1':'7B',
-            '6,1':'2B',
-            '7,1':'9B',
-            '8,1':'4B',
-            '9,1':'11B',
-            '10,1':'6B',
-            '11,1':'1B',
-            '0,0':'5A',
-            '1,0':'12A',
-            '2,0':'7A',
-            '3,0':'2A',
-            '4,0':'9A',
-            '5,0':'4A',
-            '6,0':'11A',
-            '7,0':'6A',
-            '8,0':'1A',
-            '9,0':'8A',
-            '10,0':'3A',
-            '11,0':'10A'}
+            (0,1):'8B',
+            (1,1):'3B',
+            (2,1):'10B',
+            (3,1):'5B',
+            (4,1):'12B',
+            (5,1):'7B',
+            (6,1):'2B',
+            (7,1):'9B',
+            (8,1):'4B',
+            (9,1):'11B',
+            (10,1):'6B',
+            (11,1):'1B',
+            (0,0):'5A',
+            (1,0):'12A',
+            (2,0):'7A',
+            (3,0):'2A',
+            (4,0):'9A',
+            (5,0):'4A',
+            (6,0):'11A',
+            (7,0):'6A',
+            (8,0):'1A',
+            (9,0):'8A',
+            (10,0):'3A',
+            (11,0):'10A'}
 
         camelot_key = key_dict[spotify_key]
+
         return camelot_key
 
     def get_spotify_playlists(self, user_account, print_switch):
@@ -181,6 +192,5 @@ class Playlist:
                     print("{0:4d}  {1}".format(counter,playlist['name']))
             end_index = index
             start_index = start_index+50
-        print()
 
         return(user_playlists)
