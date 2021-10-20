6cd65377138dc6b399e0c130a161cf8bca270c71
Matt Warren <52166401+matthewtwarren@users.noreply.github.com>
2021-10-20 23:00:36 +0100

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,4 +1,4 @@
-"""SpotifyDJ: Command-line interface tools for managing and creating Spotify playlists"""
+"""SortSpotify: Command-line interface tools for managing and creating Spotify playlists"""
 
 from .command_parser import CommandParser
 from .command_parser import CommandException
@@ -6,7 +6,7 @@ from .playlist_library import PlaylistLibrary
 from .user_account import UserAccount
 
 if __name__ == "__main__":
-    print("""Welcome to SpotifyDJ: A command-line interface for managing and creating Spotify playlists.\nType HELP for list of available commands or EXIT to terminate.\n""")
+    print("""Welcome to SortSpotify: A command-line interface for managing and creating Spotify playlists.\nType HELP for list of available commands or EXIT to terminate.\n""")
     playlist_library = PlaylistLibrary()
     user_account = UserAccount()
     user_account.add_user()
@@ -20,4 +20,4 @@ if __name__ == "__main__":
             parser.execute_command(command) # Might not need to use split but keep for now
         except CommandException as exception:
             print(exception)
-    print("SpotifyDJ has terminated.")
+    print("SortSpotify has terminated.")
